780c765a3d65fef718f53acaaee9c46ea9504368
codewithshamim <shamimislamonline@gmail.com>
2023-08-25 21:41:25 +0600

    validation added

--- src/app/modules/academicSemester/academicSemester.route.ts
+++ src/app/modules/academicSemester/academicSemester.route.ts
@@ -1,7 +1,13 @@
 import express, { Router } from 'express';
+import validateRequest from '../../middlewares/validateRequest';
 import { AcademicSemesterController } from './academicSemester.controller';
+import { AcademicSemesterZodValidation } from './academicSemester.validation';
 const router: Router = express.Router();
 
-router.post('/', AcademicSemesterController.createAcademicSemester);
+router.post(
+  '/',
+  validateRequest(AcademicSemesterZodValidation.create),
+  AcademicSemesterController.createAcademicSemester
+);
 
 export const AcademicSemesterRoute = router;
